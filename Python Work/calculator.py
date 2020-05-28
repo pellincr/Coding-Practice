21e9c81e43ad2e39869a073dd0f4d3ff393413ce
pellincr <44884108+pellincr@users.noreply.github.com>
2020-05-28 19:07:05 -0400

    Update calculator.py
    Working for single digits

--- Python Work/calculator.py
+++ Python Work/calculator.py
@@ -33,7 +33,7 @@ def isBalanced(eq):
     except:
         return False
 
-#get_Priortiy: string -> boolean
+#get_Priortiy: operator(string) -> boolean
 #purpose: to determine the priority of the given operator
 def get_Priority(op):
     if op == "+" or op == "-":
@@ -46,39 +46,53 @@ def get_Priority(op):
 #calc: equation(string) -> num
 #purpose: to determine what the given calculation equates to            
 def calc(eq):
-    for element in eq:
-        if element.isnumeric():
-            numstack.append(int(element))
-        elif element == "(":
-            opstack.append(element)
-        elif element == ")":
-            op = opstack.pop()
-            while(op != "("):
-                val2 = numstack.pop()
-                val1 = numstack.pop()
-                numstack.append(evaluate(val1, val2, op))
-        elif isOp(element):
-            while(opstack != [] and get_Priority(opstack[0])>= get_Priority(element)):
+    try:
+        if isBalanced(eq):
+            for element in eq:
+                #element is a number
+                if element.isnumeric():
+                    numstack.append(int(element))
+                #element is an open parenthesis
+                elif element == "(":
+                    opstack.append(element)
+                #element is a closed parenthesis
+                elif element == ")":
+                    op = opstack.pop()
+                    while(opstack != [] and op != "("):
+                        val2 = numstack.pop()
+                        val1 = numstack.pop()
+                        numstack.append(evaluate(val1, val2, op)) 
+                        op = opstack.pop() 
+                #element is an operator                      
+                elif isOp(element):
+                    while(opstack != [] and get_Priority(opstack[len(opstack)-1])>= get_Priority(element)):
+                        op = opstack.pop()
+                        val2 = numstack.pop()
+                        val1 = numstack.pop()
+                        numstack.append(evaluate(val1, val2, op))
+                    opstack.append(element)
+                elif element == " ":
+                    continue
+            #for loop ends; if not everything has been evaluated in the stack yet
+            while opstack != []:
                 op = opstack.pop()
                 val2 = numstack.pop()
                 val1 = numstack.pop()
                 numstack.append(evaluate(val1, val2, op))
-            opstack.append(element)
-    while opstack != []:
-        op = opstack.pop()
-        val2 = numstack.pop()
-        val1 = numstack.pop()
-        numstack.append(evaluate(val1, val2, op))
-    return numstack.pop()
+            return numstack.pop()
+        else:
+            return "Parentheses are not properly balanced"
+    except:
+        return "Equation not properly written"
 
 
 
 #Tests
 def test_evaluate():
-    assert evaluate(5,1,"/") == 5, "Should be 5"#5/1
-    assert evaluate(3,5,"+") == 8, "Should be 8"#3+5
-    assert evaluate(8,6,"-") == 2, "Should be 2"#8-6
-    assert evaluate(4,2,"*") == 8, "Should be 8"#4*2
+    assert evaluate(5,1,"/") == 5, "Should be 5"
+    assert evaluate(3,5,"+") == 8, "Should be 8"
+    assert evaluate(8,6,"-") == 2, "Should be 2"
+    assert evaluate(4,2,"*") == 8, "Should be 8"
 
 def test_isOp():
     assert isOp("123") == False, "Shoule be False"
@@ -97,19 +111,25 @@ def test_isBalanced():
 def test_get_Priority():
     assert get_Priority("+") == 1, "Should be 1"
     assert get_Priority("-") == 1, "Should be 1"
-    assert get_Priority("*") == 2, "Should be 1"
-    assert get_Priority("/") == 2, "Should be 1"
+    assert get_Priority("*") == 2, "Should be 2"
+    assert get_Priority("/") == 2, "Should be 2"
 
 def test_calc():
     assert calc("3+4*2") == 11, "Should be 11"
     assert calc("(3+4)*2") == 14, "Should be 14"
     assert calc("((3+4)") == "Parentheses are not properly balanced"
     assert calc("()") == "Equation not properly written"
+    assert calc("2+(5*4)") == 22, "Should be 22"
+    assert calc("(2+2)*(4+4)") == 32, "Should be 32"
+    assert calc("2+2*(4+4)") == 18, "Should be 18"
+    assert calc("2/2") == 1, "Should be 1"
+    assert calc("5*(2/2)") == 5, "Should be 5"
+    assert calc("5* (2/ 2)") == 5, "Should be 5"
 
 if __name__ == "__main__":
     test_evaluate()
     test_isOp()
     test_isBalanced()
     test_get_Priority()
-    #test_calc()
+    test_calc()
     print("All tests have passed!")
