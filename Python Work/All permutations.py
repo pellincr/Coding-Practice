9c2750a635a0dc50a96b5d20c6180f0ad38b5945
pellincr <44884108+pellincr@users.noreply.github.com>
2020-10-17 14:29:10 -0400

    Update All permutations.py
    Created permutations using the import permutations

--- Python Work/All permutations.py
+++ Python Work/All permutations.py
@@ -1,9 +1,10 @@
-#all_permutations: string list -> list-of-strings
-#purpose: to return all permutations of the given string
-def all_permutations(string, res):
-    if len(string) == 0:
-        return res
-    else:
-        place_letter(string[0:1], all_permutations(string[1], res))
+from itertools import permutations
 
-#place_letter: string list-of-strings
+#all_permutations: string -> list-of-strings
+#purpose: to output all the permutations of a given string
+def all_permutations(str):
+    return ["".join(p) for p in permutations(str)]
+
+
+if __name__ == "__main__":
+    print(all_permutations("abc"))
